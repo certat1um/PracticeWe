be805da102ebb086a6692e50bf47e9bb14ea9192
Ivan <canvasworkst@gmail.com>
2022-01-04 15:30:06 +0200

    added form validation and some responsive features for form and name choices

--- js/app.js
+++ js/app.js
@@ -2,24 +2,86 @@
 
 // == form ===>
 const formBtn = document.querySelector('.form-btn');
-const inputs = document.querySelectorAll('label input');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
 
 // btn active
 formBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    inputs.forEach((item) => {
-        item.value = '';
-    });
+    checkForm();
 });
+
+function checkForm() {
+    const firstName_value = firstName.value.trim();
+    const lastName_value = lastName.value.trim();
+    const password_value = password.value.trim();
+    const email_value = email.value.trim();
+    const phone_value = phone.value.trim();
+
+    if(firstName_value === '') {
+        setErrorFor(firstName);
+    } else {
+        let capital = firstName_value.charAt(0);
+        if(capital !== capital.toUpperCase() || capital == isNaN()) {
+            setErrorFor(firstName);
+        } else {
+            setSuccessFor(firstName);
+        }
+    }
+
+    if(lastName_value === '') {
+        setErrorFor(lastName);
+    } else {
+        let capital = lastName_value.charAt(0);
+        if(capital !== capital.toUpperCase() || capital == isNaN()) {
+            setErrorFor(lastName);
+        } else {
+            setSuccessFor(lastName);
+        }
+    }
+
+    if(password_value === '' || password_value.length < 6) {
+        setErrorFor(password);
+    } else {
+        setSuccessFor(password);
+    }
+    if(email_value === '') {
+        setErrorFor(email);
+    } else if (!isEmail(email_value)) {
+		setErrorFor(email);
+	} else {
+		setSuccessFor(email);
+	}
+    function isEmail(email) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+    if(phone_value === '' || phone_value.length !== 10) {
+        setErrorFor(phone);
+    } else {
+        setSuccessFor(phone);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formItemField = input.parentElement;
+    formItemField.classList.remove('success');
+    formItemField.classList.add('failure');
+}
+function setSuccessFor(input) {
+    const formItemField = input.parentElement;
+    formItemField.classList.remove('failure');
+    formItemField.classList.add('success');
+}
 
 // == choices ===>
 const input = document.querySelector('.naming-field input');
 const name = document.querySelectorAll('.naming-name');
 const choices = document.querySelectorAll('.naming-choice');
 const welcomePopup = document.querySelector('.naming-popup');
-
-// == welcome popup ===>
-let count;
+let count; // for welcome popup
 
 // == Name Setter in Local Storage ===>
 function fillNames() {
